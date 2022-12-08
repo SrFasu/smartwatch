9d2342389e3f452b703b8f12bf852ebb0527d200
SrFasu <facumamone65@gmail.com>
2022-12-08 00:39:43 -0300

    Horario funcional

--- src/componentes/Horario/Horario.jsx
+++ src/componentes/Horario/Horario.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 const Horario = () =>  {
+    let time = new Date().toLocaleTimeString();
+    const [, setCurrentTime] = useState(time);
+
+    const updateTime = () => {
+        let time = new Date().toLocaleTimeString();
+        setCurrentTime(time);
+    }
+
+    setInterval(updateTime);
+
+    let date = new Date().toLocaleDateString();
+
     return(
         <div className="reloj">
-        <p className="reloj__tiempo">00:00:00</p>
-        <p className="reloj__fecha">00-00-0000 Día</p>
+        <p className="reloj__tiempo">{time}</p>
+        <p className="reloj__fecha">{date}</p>
     </div>
     );
 }
