3a20a84a58e8bd28c276812e1452870c01e15c35
SrFasu <facumamone65@gmail.com>
2022-12-08 01:32:37 -0300

    cronometro funcional

--- src/componentes/Cronometro/Cronometro.jsx
+++ src/componentes/Cronometro/Cronometro.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
-const Cronometro = () =>{     
-    return(
-        <div className="crono">
-        <div className="crono__tiempo">00:00:00</div>
-        <div className="crono__controles">
-        <button id="start">Iniciar</button>
-        <button id="stop">Pausar</button>
-        <button id="reset">Parar</button>
+const Cronometro = () => {
+  const [isActive, setiIsActive] = useState(false);
+  const [ispaused, setisPaused] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive && ispaused === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, ispaused]);
+
+  const handleStart = () => {
+    setiIsActive(true);
+    setisPaused(false);
+  };
+
+  const handlePaused = () => {
+    setisPaused(!ispaused);
+  };
+
+  const handleReset = () => {
+    setiIsActive(false);
+    setTime(0);
+  };
+
+  return (
+    <div className="crono">
+
+      {console.log("time", time)}
+
+      <div className="crono__tiempo">
+          <p>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)}:{("0" + (Math.floor(time / 1000) % 60)).slice(-2)}:{("0" + (Math.floor(time / 10) % 100)).slice(-2)} </p>
+          </div>
+
+          <div className="crono__controles">
+        <button onClick={handleStart}>Iniciar</button>
+        <button onClick={handlePaused}>Pausar</button>
+        <button onClick={handleReset}>Parar</button>
         </div>
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Cronometro;
 
